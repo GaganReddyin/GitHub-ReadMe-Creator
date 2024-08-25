0d17c1a21caa407657c630e71627de00f9b60017
Gagan Reddy Mallam <99109231+GaganReddyin@users.noreply.github.com>
2024-08-25 10:33:34 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import NextNProgress from "nextjs-progressbar";
 import { green } from "tailwindcss/colors.js";
 
 const App = ({ Component, pageProps }) => {
-  return (
+  return ( 
     <>
       <NextNProgress color={green[500]} height={4} />
       <Component {...pageProps} />;
