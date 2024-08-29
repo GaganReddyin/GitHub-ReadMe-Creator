3f49cee66e189b1ebef65f64f66ad3c1b8664782
Gagan Reddy Mallam <99109231+GaganReddyin@users.noreply.github.com>
2024-08-29 14:13:27 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
       <body>  
         <Main />
         <NextScript />
-      </body>
+      </body>                                       
     </Html>
   )
 }
