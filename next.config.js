091e7d82ce14bdf57193339ed7fd822e53ca6edf
Gagan Reddy Mallam <99109231+GaganReddyin@users.noreply.github.com>
2024-09-26 12:41:42 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const { i18n } = require("./next-i18next.config");
 
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */ 
 const nextConfig = {
   reactStrictMode: true,
   i18n, 
